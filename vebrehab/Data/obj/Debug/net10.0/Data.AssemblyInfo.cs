a22f33a01ce78dd9fc96f5a2d54b7a19d2574c11
DESKTOP-KBMHMK6\danil <a>
2026-02-04 23:16:51 +0500

    #16 пересобрал вебморду с мудблазором

--- vebrehab/Data/obj/Debug/net10.0/Data.AssemblyInfo.cs
+++ vebrehab/Data/obj/Debug/net10.0/Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e958eeb4b50623b26e53eaadf6a42bea82d5b344")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c83cdeb58d8d977233795f08a79242d4a551426")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
